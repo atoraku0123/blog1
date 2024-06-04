5ac4b6f120e6dbed5b6c026af81e15e2514a03e1
atoraku <cfyt0123@gmail.com>
2024-06-04 02:55:06 +0000

    python-venvブログ用ソースcommit

---
+++ python-venv/main.py
@@ -0,0 +1,16 @@
+# python-dotenvをimportします。
+from dotenv import load_dotenv
+import os
+
+# 同一ディレクトリに配置されている「.env」ファイルから環境変数を読み込みます。
+load_dotenv()
+
+# 「.env」ファイルのtest envの値を読み込みます。
+TEST_ENV: str = os.environ.get("TEST_ENV")
+ 
+if TEST_ENV == "":
+    # dotenvのimprotに失敗していればこちらが出力されます。
+    print("TEST_ENV not read")
+else:
+    # dotenvのimprotに成功し「.env」の環境変数が読み込めていればこちらが出力されます。
+    print("TEST_ENV value is {}".format(TEST_ENV))
