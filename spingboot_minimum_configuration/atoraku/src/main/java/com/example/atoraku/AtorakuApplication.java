a5de716baac7704e858ef87c99279951c531f2dd
atoraku0123 <cfyt0123@gmail.com>
2024-07-22 23:21:29 +0900

    spingboot_minimum_configuration 修正途中

---
+++ spingboot_minimum_configuration/atoraku/src/main/java/com/example/atoraku/AtorakuApplication.java
@@ -0,0 +1,13 @@
+package com.example.atoraku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtorakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtorakuApplication.class, args);
+	}
+
+}
---
+++ spingboot_minimum_configuration/atoraku/src/test/java/com/example/atoraku/AtorakuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atoraku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtorakuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
