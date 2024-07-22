672742a4d39125cc580c8666d6aad6b0f1ab8d00
atoraku0123 <cfyt0123@gmail.com>
2024-07-23 01:50:02 +0900

    a

--- spingboot_minimum_configuration/atoraku/src/main/java/com/example/atoraku/controller/SweetsController.java
+++ spingboot_minimum_configuration/atoraku/src/main/java/com/example/atoraku/controller/SweetsController.java
@@ -1,5 +1,14 @@
 package com.example.atoraku.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SweetsController {
+
+    @GetMapping("/")
+    public String getSweets() {
+        return "test";
+    }
 
 }
