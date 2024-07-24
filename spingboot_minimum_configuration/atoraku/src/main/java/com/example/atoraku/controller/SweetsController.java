805f0c853368fecd82612d4b0c31ce68278bc08b
atoraku0123 <cfyt0123@gmail.com>
2024-07-24 22:57:19 +0900

    a

--- spingboot_minimum_configuration/atoraku/src/main/java/com/example/atoraku/controller/SweetsController.java
+++ spingboot_minimum_configuration/atoraku/src/main/java/com/example/atoraku/controller/SweetsController.java
@@ -1,15 +1,6 @@
-package com.example.atoraku.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@
 public class SweetsController {
-
-    @GetMapping("/sweets")
-    public ResponseEntity<String> getSweets() {
-        return ResponseEntity.ok("test\n");
-    }
 
 }
